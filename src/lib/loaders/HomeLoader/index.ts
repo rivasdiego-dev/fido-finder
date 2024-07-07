929e6ad56a3e743f8020e7dc36d931a1b8019371
José Miguel Acosta Vásquez <62571916+Acostam331@users.noreply.github.com>
2024-07-06 22:23:35 -0600

    :beers: Array optimization and sort

--- src/lib/loaders/HomeLoader/index.ts
+++ src/lib/loaders/HomeLoader/index.ts
@@ -5,7 +5,13 @@ export default async function HomeLoader() {
 
   if (postsResponse.isError) return { posts: [] };
 
-  const posts = postsResponse.response.data as ApiPost[];
+  const rawPosts = postsResponse.response.data as ApiPost[];
+
+  const posts = rawPosts.sort((a, b) => {
+    return (
+      new Date(b.lost_datetime).getTime() - new Date(a.lost_datetime).getTime()
+    );
+  });
 
   return { posts };
 }
--- src/lib/loaders/SearchLoader/index.ts
+++ src/lib/loaders/SearchLoader/index.ts
@@ -5,7 +5,15 @@ export default async function SearchLoader() {
 
   if (postsResponse.isError) return [];
 
-  const posts = postsResponse.response.data as ApiPost[];
+  const rawPosts = postsResponse.response.data as ApiPost[];
+
+  console.log(rawPosts);
+
+  const posts = rawPosts.sort((a, b) => {
+    return (
+      new Date(b.lost_datetime).getTime() - new Date(a.lost_datetime).getTime()
+    );
+  });
 
   return posts;
 }
